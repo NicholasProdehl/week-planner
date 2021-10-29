13b0136e4f76a573a0cb9f8bc14c07c70af9366c
Nicholas Prodehl <nicholasprodehl@gmail.com>
2021-10-29 18:00:12 -0500

    Up up and away.

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var $addEntry = document.querySelector('.addEntryBtn');
 var $submit = document.querySelector('#submit');
 var $modalContainer = document.getElementById('modalContainer');
+var $form = document.forms[0];
 
 $addEntry.addEventListener('click', function (event) {
   $modalContainer.className = 'container';
 });
+var dataObj = {};
 
 $submit.addEventListener('click', function (event) {
   $modalContainer.className = 'hidden';
+  dataObj.dayOfWeek = document.getElementById('dayOfWeek').value;
+  dataObj.time = document.getElementById('time').value;
+  dataObj.textInput = document.getElementById('textInput').value;
+  console.log(dataObj);
+  event.preventDefault();
+});
+
+window.addEventListener('submit', function (event) {
+
 });
